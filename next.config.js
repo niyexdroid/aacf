7bcfeb004a0094e18a78059bb2c094d62d12c197
root <root@ip-172-31-35-238.ap-south-1.compute.internal>
2025-04-06 20:55:40 +0000

    Update next.config.js with standardized configuration

--- next.config.js
+++ next.config.js
@@ -1,31 +1,24 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
-
 /** @type {import("next").NextConfig} */
 const config = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '*',
-				pathname: '**',
-			},
-		],
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	webpack: (config, { isServer }) => {
-		config.stats = "verbose";
-		return config;
-	},
-	output: "export"
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: '*',
+            pathname: '**',
+        }, ],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    missingSuspenseWithCSRBailout: false,
+    webpack: (config, { isServer }) => {
+        config.stats = "verbose";
+        return config;
+    },
 };
-
 export default config;
