81a4f386b89909a611475c3550b5e8429ff331bb
niyexdroid <adeniyi.4568@yahoo.com>
2025-04-21 13:57:32 +0100

    All the apikeys and sensitive information has been hidden in the dotenv file

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBrUUEGvN_RShjJvv75kseENYf82F19mH0",
-  authDomain: "aacf-1434c.firebaseapp.com",
-  projectId: "aacf-1434c",
-  storageBucket: "aacf-1434c.firebasestorage.app",
-  messagingSenderId: "854288563982",
-  appId: "1:854288563982:web:be0d70fd8fb22f94d8615f",
-  measurementId: "G-HWBV1VM9TZ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
